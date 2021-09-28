61621a1520bc6cbbbbf6a367aed207c01b88831e
Sebastian Cespedes <sebastian.2305@gotmail.com>
2021-09-27 20:07:00 -0500

    App base

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :employees
+  #root 'employees#new'
+  get "landing/index"  
+  root to: "landing#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
